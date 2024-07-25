f8cc68c019cd60a4a9abd6f4abf93f4780403ac2
lohikrme <chinese.parrotlet@gmail.com>
2024-07-25 13:25:45 +0300

    fixes

--- ludusbot.py
+++ ludusbot.py
@@ -1,27 +1,26 @@
 # ludusbot.py
-# Made by Chinese Parrot 24th july 2024
+# Made by Chinese Parrot 25th july 2024
 
 import random
 import psycopg2
 import discord
 from discord.ext import commands
 
-# create a default intents object
+# GIVE BOT DEFAULT INTENTS + ACCESS TO MESSAGE CONTENT AND MEMBERS AND SET COMMAND PREFIX TO BE '%'
 intents = discord.Intents.default()  
 intents.message_content = True 
 intents.members = True
 
-# add both command prefix and intents to the bot
 bot = commands.Bot(command_prefix='%', intents=intents)  # use percent as start of command
 
-# when bot enters the channel
+# BOT ENTERS THE CHANNEL
 @bot.event
 async def on_ready():
     print(f'Logged in as {bot.user} (ID: {bot.user.id})')
 
 # --------- ALL COMMANDS ---------------
 
-# factual command
+# FACTUAL COMMAND
 @bot.command()
 async def factual(ctx):
     commands = ["Chinese Parrot is a tier S duellist.", 
@@ -39,18 +38,18 @@ async def factual(ctx):
     answer = random.choice(commands)
     await ctx.send(answer)
 
-# register command
+# REGISTER COMMAND
 @bot.command()
 async def register(ctx, nickname):
 
-    username = ctx.author.name
+    # FETCH USER'S OFFICIAL DISCORD NAME
+    username = str(ctx.author.name)
 
-    # fetch database password
+    # OPEN CONNECTION TO DATABASE
     file = open('dbpassword.txt', 'r')
     salasana = file.readline()
     file.close()
 
-    # open connection to database
     conn = psycopg2.connect(
         database = "ludus",
         user = "postgres",
@@ -59,7 +58,7 @@ async def register(ctx, nickname):
         port = "5432"
     )
 
-    # if user has alrdy been registered
+    # NOTIFY USER THEY ARE ALREADY REGISTERED
     cursor = conn.cursor()
     cursor.execute("SELECT * FROM players WHERE username = %s", (username,))
     existing_user = cursor.fetchone()
@@ -67,24 +66,26 @@ async def register(ctx, nickname):
     if existing_user is not None:
         await ctx.send("You have already been registered! If you want to reset your rank, please contact Ludus admins.")
 
-    # add a new user if database has space
+    # ADD A NEW USER
     else:
         cursor = conn.cursor()
         cursor.execute("SELECT COUNT(*) FROM players", ())
         amount_of_lines = cursor.fetchone()[0]
         if (amount_of_lines < 10000):
             cursor = conn.cursor()
-            cursor.execute("INSERT INTO players (username, points, nickname) VALUES (%s, %s)", (username, 0, nickname))
+            cursor.execute("INSERT INTO players (username, points, nickname) VALUES (%s, %s, %s)", (username, 0, nickname))
             await ctx.send("Your discord username has successfully been registered into Ludus players!")
         else:
             await ctx.send("The database is full. This most likely means that the database has been attacked by a spammer. \n This will not endanger security, but limit makes sure database wont grow too large. Please contact Ludus admins.")
 
-# queue command
+# CHALLENGE COMMAND
 
-# rename command
+# RENAME COMMAND
+
+# lEADERBOARD COMMAND
 
 
-# token of bot to identify
+# TOKEN OF BOT TO IDENTIFY AND USE IN CHANNELS
 file = open('token.txt', 'r')
 token = file.readline()
 file.close()
