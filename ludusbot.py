4477e512bea54d8cf7e6c98d5473360cc9c22004
lohikrme <chinese.parrotlet@gmail.com>
2024-07-26 22:24:34 +0300

    leaderboard and top10 commands work

--- ludusbot.py
+++ ludusbot.py
@@ -4,6 +4,7 @@
 import random
 import psycopg2
 import discord
+import settings
 from discord.ext import commands
 
 # GIVE BOT DEFAULT INTENTS + ACCESS TO MESSAGE CONTENT AND MEMBERS AND SET COMMAND PREFIX TO BE '%'
@@ -11,7 +12,18 @@ intents = discord.Intents.default()
 intents.message_content = True 
 intents.members = True
 
-bot = commands.Bot(command_prefix='%', intents=intents)  # use percent as start of command
+bot = commands.Bot(command_prefix='/', intents=intents)  # use slash as start of command
+
+# OPEN THE CONNECTION TO THE DATABASE
+
+conn = psycopg2.connect(
+        database = settings.db_name,
+        user = settings.db_user,
+        password = settings.db_password,
+        host = settings.db_host,
+        port = settings.db_port
+    )
+conn.autocommit = True
 
 # BOT ENTERS THE CHANNEL
 @bot.event
@@ -33,30 +45,27 @@ async def factual(ctx):
                 "You can calculate the 95% Confidence interval using formula: [average - 1.96 * SD; average + 1.96 * SD]",
                 "Rabbits despite their smaller size usually dominate larger hares when competing territory",
                 "The flying squirrel is a cute fluffy mammal that glides from tree to tree, eats leaves, and is protected by the EU directive",
-                "Psychopaths don't feel anxiety or remorse, so if u feel neither, most likely u are not a psychopath"
+                "Psychopaths don't feel anxiety or remorse. If you are anxious, you are not a real psychopath.",
+                "Xoxoxoxo",
+                "Top 5 players of ludus are: Maximou, Totenflag, Parrot, Shin, Nerji",
+                "Perch (Perca fluviatilis) is the most common fish in Europe",
+                "Crucian carp (Carassius carassius) is the most badass fish out there. They can survive months without breathing during the harsh winters when small lakes freeze all the way to bottom.",
+                "The origin of house cat is in the African wild cats (Felis sylvestris lybica)",
+                "Most kinds of evolution are actually devolution - loss of genetic material. This is how dogs or MRSA developed.",
+                "The heaviest sumo ever was Orora weighting up to 292kg with a height of 190cm",
+                "Some parrots such as cockatoos, macaws and conures can produce up to 135 decibel sound and induce immediate hearing damage to humans.",
+                "Airplane gasolin still contains lead in 2024 because the gasolin freezes so much in the air that ethanol cannot be used. Living nearby airfields can therefore be dangerous.",
+
                 ]
+    
     answer = random.choice(commands)
     await ctx.send(answer)
 
 # REGISTER COMMAND
 @bot.command()
 async def register(ctx, nickname):
-
     # FETCH USER'S OFFICIAL DISCORD NAME
     username = str(ctx.author.name)
-
-    # OPEN CONNECTION TO DATABASE
-    file = open('dbpassword.txt', 'r')
-    salasana = file.readline()
-    file.close()
-
-    conn = psycopg2.connect(
-        database = "ludus",
-        user = "postgres",
-        password = salasana,
-        host = "localhost",
-        port = "5432"
-    )
 
     # NOTIFY USER THEY ARE ALREADY REGISTERED
     cursor = conn.cursor()
@@ -78,16 +87,76 @@ async def register(ctx, nickname):
         else:
             await ctx.send("The database is full. This most likely means that the database has been attacked by a spammer. \n This will not endanger security, but limit makes sure database wont grow too large. Please contact Ludus admins.")
 
-# CHALLENGE COMMAND
 
-# RENAME COMMAND
+# CHANGENICKNAME COMMAND
+@bot.command()
+async def changeNickName(ctx, nickname):
+
+    # FETCH USER'S OFFICIAL DISCORD NAME
+    username = str(ctx.author.name)
+
+    # IF USERNAME EXISTS, CHANGE THEIR NICKNAME
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM players WHERE username = %s", (username,))
+    existing_user = cursor.fetchone()
+
+    if existing_user is not None:
+        cursor = conn.cursor()
+        cursor.execute("SELECT nickname FROM players WHERE username = %s", (username,))
+        old_nickname = cursor.fetchone()[0]
+        cursor.execute("UPDATE players SET nickname = %s WHERE username = %s", (nickname, username))
+        await ctx.send(f"Your nickname has been updated! Your old nickname was {old_nickname}. Your new nickname is {nickname}")
+
 
 # lEADERBOARD COMMAND
+@bot.command()
+async def leaderboard(ctx):
+    await ctx.send("Printing points of all players starting...")
+
+    scoreboard_text = "SCOREBOARD"
+    await ctx.send("```**" + scoreboard_text.center(24) + "**```")
+
+    cursor = conn.cursor()
+    cursor.execute("SELECT nickname, points FROM players ORDER BY points DESC",())
+
+    messages = []
+    data = cursor.fetchmany(1000)
+    for item in data:
+        printable_text = str(item)
+        messages.append("```**" + printable_text.center(24) + "**```")
+    await ctx.send("\n".join(messages))
+    await ctx.send("All players have been printed!")
+
+
+# TOP10 COMMAND
+@bot.command()
+async def top10(ctx):
+    await ctx.send("Printing points of top10 players starting...")
+    
+    scoreboard_text = "TOP10 PLAYERS:"
+    await ctx.send(f"```**{scoreboard_text.center(24)}**```")
+    
+    cursor = conn.cursor()
+    cursor.execute("SELECT nickname, points FROM players ORDER BY points DESC LIMIT 10")
+    
+    messages = []
+    data = cursor.fetchmany(10)
+    for row in data:
+        printable_text = str(row)
+        messages.append(f"```**{printable_text.center(24)}**```")
+    
+    await ctx.send("\n".join(messages))
+    await ctx.send("Top10 players have been printed!")
+    
+    cursor.close()
+
+
+
+# CHALLENGE COMMAND
+
+
 
 
 # TOKEN OF BOT TO IDENTIFY AND USE IN CHANNELS
-file = open('token.txt', 'r')
-token = file.readline()
-file.close()
-
+token = settings.bot_token
 bot.run(token)
