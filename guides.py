aabbfd0a2abfe71380b899b6bcbc1c60b4f3f6df
lohikrme <chinese.parrotlet@gmail.com>
2024-09-10 14:32:46 +0300

    guides now look better

--- guides.py
+++ guides.py
@@ -1,5 +1,5 @@
 
-guide_eng = """'Ludus ranking bot' has 3 basic functionalities:
+guide_eng = """```'Ludus ranking bot' has 3 basic functionalities:
 
 1. The clans gain ranking points and save their clanwar scores by using the '/reportclanwar' command.
 The clan must have an admin registered with the '/registeradmin' command who will do it.
@@ -10,12 +10,12 @@ These clan scores can be printed with '/printclanwars' or '/leaderboardclans'.
 The command contains a 'role' which defines to whom the bot will directly message about the event.
 
 3. Players can use the '/challenge' to gain personal ranking points e.g after duels.
-These scores can be printed with '/leaderboardplayers' or '/leaderboardplayersofclan'"""
+These scores can be printed with '/leaderboardplayers' or '/leaderboardplayersofclan'```"""
 
 
 
 
-guide_rus = """'Ludus ranking bot' имеет 3 основные функции:
+guide_rus = """```'Ludus ranking bot' имеет 3 основные функции:
 
 1. Кланы получают очки рейтинга и сохраняют свои результаты клановых войн, используя команду '/reportclanwar'.
 Клан должен иметь администратора, зарегистрированного с помощью команды '/registeradmin', который будет это делать.
@@ -26,13 +26,13 @@ guide_rus = """'Ludus ranking bot' имеет 3 основные функции:
 Команда содержит 'role', который определяет, кому бот будет напрямую отправлять сообщения о событии.
 
 3. Игроки могут использовать команду '/challenge' для получения личных очков рейтинга, например, после дуэлей.
-Эти результаты можно распечатать с помощью команд '/leaderboardplayers' или '/leaderboardplayersofclan'"""
+Эти результаты можно распечатать с помощью команд '/leaderboardplayers' или '/leaderboardplayersofclan'```"""
 
 
 
 
 
-guide_tr = """'Ludus ranking bot' 3 temel işlevi vardır:
+guide_tr = """```'Ludus ranking bot' 3 temel işlevi vardır:
 
 1. Klanlar, '/reportclanwar' komutunu kullanarak sıralama puanları kazanır ve klan savaşı skorlarını kaydeder.
 Klanın, bunu yapacak olan '/registeradmin' komutuyla kayıtlı bir yöneticisi olmalıdır.
@@ -43,4 +43,4 @@ Bu klan skorları '/printclanwars' veya '/leaderboardclans' komutlarıyla yazdı
 Komut, botun etkinlik hakkında doğrudan mesaj göndereceği kişileri belirleyen bir 'role' içerir.
 
 3. Oyuncular, düellolar gibi kişisel sıralama puanları kazanmak için '/challenge' komutunu kullanabilir.
-Bu skorlar '/leaderboardplayers' veya '/leaderboardplayersofclan' komutlarıyla yazdırılabilir."""
+Bu skorlar '/leaderboardplayers' veya '/leaderboardplayersofclan' komutlarıyla yazdırılabilir.```"""
