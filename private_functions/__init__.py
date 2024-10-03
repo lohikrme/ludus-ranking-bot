7335eb37e0653b44065bb463c842b08986c0b644
lohikrme <chinese.parrotlet@gmail.com>
2024-10-03 10:32:53 +0300

    moved a private function into printmyft7.py file. left as priv func update playerpoints / clanpoints / duelhistory for future server API purposes

--- private_functions/__init__.py
+++ private_functions/__init__.py
@@ -1,6 +1,5 @@
 from .fetch_existing_clannames import _fetch_existing_clannames
 from .is_registered import _is_registered
-from .print_myft7s_against import _print_myft7s_against
 from .update_clan_points import _update_clan_points
 from .update_duels_history import _update_duels_history
 from .update_player_points import _update_player_points
