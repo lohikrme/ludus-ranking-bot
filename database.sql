3e7bde117be54616670e73e37686cd4c1804b0b4
lohikrme <chinese.parrotlet@gmail.com>
2024-10-06 19:04:53 +0300

    foreign keys updated into the database

--- database.sql
+++ database.sql
@@ -14,7 +14,8 @@ CREATE TABLE players (
     battles INT DEFAULT 0 NOT NULL,
     wins INT DEFAULT 0 NOT NULL,
     average_enemy_rank NUMERIC(12, 6) DEFAULT 1000.000000 NOT NULL,
-    clan_id INT DEFAULT 1);
+    clan_id INT DEFAULT 1,
+    FOREIGN KEY (clan_id) REFERENCES clans(id));
 
 -- after creating clabs, add one clan with id = 1, clanname = "none", because some commands are based on on "old clan"
 CREATE TABLE clans (
@@ -34,11 +35,14 @@ CREATE TABLE clanwars (
     challenger_clan_id INT NOT NULL,
     opponent_clan_id INT NOT NULL,
     challenger_score INT DEFAULT 0 NOT NULL,
-    opponent_score INT DEFAULT 0 NOT NULL);
+    opponent_score INT DEFAULT 0 NOT NULL,
+    FOREIGN KEY (challenger_clan_id) REFERENCES clans(id),
+    FOREIGN KEY (opponent_clan_id) REFERENCES clans(id));
 
 CREATE TABLE admins (
     id SERIAL PRIMARY KEY,
-    discord_id VARCHAR(100) NOT NULL);
+    discord_id VARCHAR(100) NOT NULL,
+    FOREIGN KEY (discord_id) REFERENCES players(discord_id));
 
 
 -- example: 
@@ -50,8 +54,9 @@ CREATE TABLE duels (
     challenger_discord_id VARCHAR(100) NOT NULL,
     opponent_discord_id VARCHAR(100) NOT NULL,
     challenger_score INT NOT NULL,
-    opponent_score INT NOT NULL
-);
+    opponent_score INT NOT NULL,
+    FOREIGN KEY (challenger_discord_id) REFERENCES players(discord_id),
+    FOREIGN KEY (opponent_discord_id) REFERENCES players(discord_id));
 
 
 CREATE TABLE guilds (
@@ -59,6 +64,7 @@ CREATE TABLE guilds (
     name VARCHAR(100) NOT NULL
 );
 
+-- so far 3 discord servers are using the bot, in future marchia and others will join
 INSERT INTO guilds(id, name) VALUES ('1060303582487908423', 'middle-earth');
 INSERT INTO guilds(id, name) VALUES ('1194360639544635482', 'legion');
 INSERT INTO guilds(id, name) VALUES ('1274726630668894249', 'valkyrie');
